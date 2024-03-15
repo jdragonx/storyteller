ae3aa22c494cb2d53a9820b92856b3997931994b
Jonathan Herrera <jonherr21@gmail.com>
2024-03-14 21:58:17 -0500

    Puesto GPT4 y reduce el largo de las historias de forma automatica

--- terror/creador_historias.py
+++ terror/creador_historias.py
@@ -128,11 +128,23 @@ def create_story(short: bool, do_trends: bool):
     ]
 
     trends = [
-        'deep web',
-        'free fire',
-        'ciudad abandonada',
-        'minecraft',
-        'megamente',
+        'amorío',
+        'videojuego',
+        'enfermo mental',
+        'párroco',
+        'religión',
+        'tema_removido',
+        'niño obeso',
+        'tema_removido',
+        'tema_removido',
+        'eutanasia',
+        'pena de muerte',
+        'tema_removido',
+        'tema_removido',
+        'feminista',
+        'gay',
+        'transexual',
+        'tema_removido',
     ]
 
     n_details = 1 if do_trends else random.randint(0, 1)
@@ -141,7 +153,7 @@ def create_story(short: bool, do_trends: bool):
     details_instructions = f", siempre te aseguras de poner todos los siguientes detalles en el outline: {', '.join(details).lower()}" if len(details) else ""
     format_instructions = '{"outline": "aquí pones el outline, de forma seguida y continua, sin bullet points ni numeración.", "titulo": "aquí pondrás el título de la historia, debes tener en cuenta que debe ser un título muy intrigante, el cual llame la atención enseguida con solo verlo y obligue a las personas abrir la historia para saber de qué se trata. La historia será publicada online por lo que el título es extremadamente importante, piensa que será narrada en un video de youtube, por eso el título debe ser el que daría los mejores resultados para que los usuarios de youtube que lo vean den click en el video. Además debe ser un título corto, máximo de 7 palabras."}'
 
-    longitud = " La historia va a ser leída con una duración de entre 10 a 15 minutos y tendrá entre 1000 y 3000 palabras, con varios párrafos." if not short else " La historia va a ser leída con una duración de 30 segundos y tendrá muy pocas palabras, con un único párrafo."
+    longitud = " La historia va a ser leída con una duración de entre 10 a 15 minutos y tendrá entre 1000 y 3000 palabras, con varios párrafos." if not short else " La historia va a ser leída con una duración de 30 segundos y tendrá alrededor de 100 palabras, con un único párrafo."
 
     messages = [
       {'role': 'system', 'content': f'Eres un chabot que crea un outline para una historia de terror{details_instructions}.{longitud}\n\nDebes tener en cuenta que la historia sea totalmente escalofriante, con sucesos inesperados y originales, dejando de lado los clásicos clichés y temáticas del terror, y usando nombres completamente inventados para los personajes que no evoquen a otras historias o peliculas. Añade diálogos cuando creas necesario. NUNCA ESCRIBES FRASES NI ORACIONES REDUNDANTES. Siempre narras tus historias en {persona_narracion}.{tipo_de_final} Siempre evitas cometer errores ortográficos o gramaticales en tu historia, ni siquiera en el título, toda la redacción es siempre perfecta. Tus historias siempre están escritas en tiempo pasado, escribes con el estilo de un estudiante de colegio. Tu respuesta es en formato json, usas el siguiente formato, este formato será utilizado en python y parseado automáticamente. Siempre envías el formato correcto, el cual sigue estas directrices: {format_instructions}'},
@@ -169,12 +181,12 @@ def create_story(short: bool, do_trends: bool):
     print("Creando historia...")
     format_instructions_historia = '{"titulo": "aquí pondrás el título de la historia", "historia": "Aquí pones la historia."}'
     messages = [
-      {'role': 'system', 'content': f'Eres un chabot que crea una historia de terror a partir de un outline y un título.{longitud} Expande cada tema del outline en su totalidad y con extremo detalle. El título de la historia es "{titulo}", y el outline es el siguiente: "{outline}".\n\n\nDebes tener en cuenta que la historia sea totalmente escalofriante, con sucesos inesperados y originales, dejando de lado los clásicos clichés y temáticas del terror, y usando nombres completamente inventados para los personajes que no evoquen a otras historias o peliculas. Añade diálogos cuando creas necesario. SIEMPRE EVITAS ESCRIBIR FRASES NI ORACIONES REDUNDANTES. Siempre narras tus historias en {persona_narracion}.{tipo_de_final} Ten en cuenta que esta es la historia final, así que siempre evitas usar frases descriptivas como "el protagonista", "la historia termina con...", "dejando al lector...", etc., ya que no estás describiendo algo para alguien más, estás mostrando la historia al lector final. Siempre evitas cometer errores ortográficos o gramaticales en tu historia, ni siquiera en el título, toda la redacción es siempre perfecta, y siempre en idioma español sin errores de codificación. Tus historias siempre están escritas en tiempo pasado, escribes con el estilo de un estudiante de colegio. Tu respuesta es en formato json, usas el siguiente formato, este formato será utilizado en python y parseado automáticamente. Siempre envías el formato correcto, el cual sigue estas directrices: {format_instructions_historia}'},
+      {'role': 'system', 'content': f'Eres un chabot que crea una historia de terror a partir de un outline y un título.{longitud} Expande cada tema del outline en su totalidad y con extremo detalle. El título de la historia es "{titulo}", y el outline es el siguiente: "{outline}".\n\n\nDebes tener en cuenta que la historia sea totalmente escalofriante, con sucesos inesperados y originales, dejando de lado los clásicos clichés y temáticas del terror, y usando nombres completamente inventados para los personajes que no evoquen a otras historias o peliculas. Añade diálogos cuando creas necesario. SIEMPRE EVITAS ESCRIBIR FRASES Y ORACIONES REDUNDANTES. Siempre narras tus historias en {persona_narracion}.{tipo_de_final} Ten en cuenta que esta es la historia final, así que siempre evitas usar frases descriptivas como "el protagonista", "la historia termina con...", "dejando al lector...", etc., ya que no estás describiendo algo para alguien más, estás mostrando la historia al lector final. Siempre evitas cometer errores ortográficos o gramaticales en tu historia, ni siquiera en el título, toda la redacción es siempre perfecta, y siempre en idioma español sin errores de codificación. Tus historias siempre están escritas en tiempo pasado, escribes con el estilo de un estudiante de colegio. Tu respuesta es en formato json, usas el siguiente formato, este formato será utilizado en python y parseado automáticamente. Siempre envías el formato correcto, el cual sigue estas directrices: {format_instructions_historia}'},
     ]
     
     try:
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo-0125",
+            model="gpt-4-0125-preview",
             messages=messages,
             response_format={"type": "json_object"},
             max_tokens=4000,
@@ -187,10 +199,14 @@ def create_story(short: bool, do_trends: bool):
         print(f"Error creating chat completion: {e}")
         return
     
-    # Increase the length of the story if it's too short
+    # Either increase the length of the story if it's too short, or reduce it if it's too long, depending on the 'short' parameter
     current_attempt = 0
     while historia and (len(re.findall(r'\b\w+\b', historia)) < 500) and not short and current_attempt < 10:
         historia = increase_length(historia)
+        current_attempt += 1
+    
+    while historia and (len(re.findall(r'\b\w+\b', historia)) > 110) and short and current_attempt < 10:
+        historia = reduce_length(historia)
         current_attempt += 1
 
     # Define the base file name
@@ -226,7 +242,7 @@ def increase_length(historia: str):
     
     try:
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo-0125",
+            model="gpt-4-0125-preview",
             messages=messages,
             response_format={"type": "json_object"},
             max_tokens=4000,
@@ -240,6 +256,29 @@ def increase_length(historia: str):
         print(f"Error creating chat completion: {e}")
         return historia
 
+def reduce_length(historia: str):
+    # Reducción de la historia
+    print("Reduciendo historia...")
+    format_instructions_historia_corta = '{"historia_reducida": "aquí pones la historia reducida, recuerda que debe ser más corta que la original, debe tener alrededor de 100 palabras, con un único párrafo."}'
+    messages = [
+      {'role': 'system', 'content': f'Eres un escritor famoso, tomas una historia grande e instantáneamente la transformas en una historia pequeña que se llena de comentarios, admiración y mucha popularidad. Para lograr eso siempre te enfocas en que las historias grandes sean reducidas en historias pequeñas, pero conversen su esencia, no cambias el tipo de historia, ni el desenlace, creas grandes escenarios y sucesos que narras para lograr que la atmósfera atrape al lector y lo lleve al mundo de la historia como si fuera real. Siempre narras tus historias en {persona_narracion}.{tipo_de_final} Ten en cuenta que esta es la historia final, así que siempre evitas usar frases descriptivas como "el protagonista", "la historia termina con...", "dejando al lector...", etc., ya que no estás describiendo algo para alguien más, estás mostrando la historia al lector final. Siempre evitas cometer errores ortográficos o gramaticales en tu historia, ni siquiera en el título, toda la redacción es siempre perfecta, y siempre en idioma español sin errores de codificación. Tus historias siempre están escritas en tiempo pasado, escribes con el estilo de un estudiante de colegio. Siempre respondes en formato json, el cual es un formato perfecto y puede ser parseado directamente en python. Usas un único párrafo para dar mayor facilidad a la lectura de tus historias. El formato de tu respuesta es el siguiente: {format_instructions_historia_corta}\n\nLa historia es la siguiente:\n\n\n"{historia}"'},
+    ]
+
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4-0125-preview",
+            messages=messages,
+            response_format={"type": "json_object"},
+            max_tokens=4000,
+            temperature=0,
+            seed=datetime.datetime.now().timestamp().__int__()
+        )
+        parsed_response = json.loads(response.choices[0].message.content)
+        historia_reducida = parsed_response['historia_reducida']
+        return historia_reducida
+    except Exception as e:
+        print(f"Error creating chat completion: {e}")
+        return historia
 
 if __name__ == '__main__':
     main()
