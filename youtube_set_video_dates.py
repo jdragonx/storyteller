ea2c2402599116aabedd8a792c6b5b02938f1c54
Jonathan Herrera <jonherr21@gmail.com>
2024-09-20 10:45:57 -0500

    wait a little longer for video thumbnail

--- youtube_set_video_dates.py
+++ youtube_set_video_dates.py
@@ -58,9 +58,10 @@ def set_video_data(driver: webdriver.Chrome, wait: WebDriverWait, is_first: bool
     # Search for the video
     search_video_input = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="search-yours"]')))
     search_video_input.send_keys("El festín de la isla oscura #terror #miedo")
+    time.sleep(10)
 
     # Click on the video
-    video_to_import = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/ytcp-video-pick-dialog/ytcp-dialog/tp-yt-paper-dialog/div[2]/div/ytcp-video-pick-dialog-contents/div/div/div/ytcp-entity-card/div')))
+    video_to_import = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="content"]')))
     ActionChains(driver).move_to_element(video_to_import).click().perform()
     time.sleep(10)
 
